5a47f19f5a8b7a6ff8bf35a693667f1c1c113548
kroma2 <janickijakub05@gmail.com>
2025-08-30 18:17:55 +0200

    fix divs id naming

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const containerDiv = document.querySelector("#container");
 
-for(let c = 0; c < 16; c++){
-    let col = document.createElement("div");
-    col.setAttribute("id", "column")
-    containerDiv.appendChild(col)
+for(let r = 0; r < 16; r++){
+    let row = document.createElement("div");
+    row.setAttribute("id", "row")
+    containerDiv.appendChild(row)
 }
 
 
-const columnDivs = document.querySelectorAll("#column");
-columnDivs.forEach(columnDiv =>{
-    for(let r = 0; r < 16; r++){
-        let row = document.createElement("div")
-        row.setAttribute("id", "row")
-        columnDiv.appendChild(row);
+const rowDivs = document.querySelectorAll("#row");
+rowDivs.forEach(rowDiv =>{
+    for(let c = 0; c < 16; c++){
+        let col = document.createElement("div")
+        col.setAttribute("id", "column")
+        rowDiv.appendChild(col);
     }
 })
