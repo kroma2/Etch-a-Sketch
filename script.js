8a7423dad92264eed46bc997d09c9336fd5be054
kroma2 <janickijakub05@gmail.com>
2025-08-30 18:32:20 +0200

    change id to class

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const containerDiv = document.querySelector("#container");
 
 for(let r = 0; r < 16; r++){
     let row = document.createElement("div");
-    row.setAttribute("id", "row")
-    containerDiv.appendChild(row)
+    row.setAttribute("class", "row");
+    containerDiv.appendChild(row);
 }
 
 
-const rowDivs = document.querySelectorAll("#row");
+const rowDivs = document.querySelectorAll(".row");
 rowDivs.forEach(rowDiv =>{
     for(let c = 0; c < 16; c++){
-        let col = document.createElement("div")
-        col.setAttribute("id", "column")
+        let col = document.createElement("div");
+        col.setAttribute("class", "column");
         rowDiv.appendChild(col);
     }
 })
